3e02f84bdeb377581a272f3b4d971bea45f5651f
malxxy <maliayraguen@gmail.com>
2023-04-14 17:45:29 -0700

    install tailwind

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'lightPink': '#FAE1DF',
+      'dark': '#0D1F2D',
+      'blueGrey': '#546A7B',
+      'trueGrey': '#9EA3B0',
+      'light': '#F7F9F9'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
